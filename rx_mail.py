a01ccc0a2e6dea14775adca03cfc9b5e784bd21e
F8ASB <f8asb@hotmail.fr>
2022-09-11 13:15:05 +0200

    Update rx_mail.py

--- rx_mail.py
+++ rx_mail.py
@@ -1,6 +1,6 @@
 from imap_tools import MailBox, AND
 
-# Get date, subject and body len of all emails from INBOX folder
+# reception des nouveaux mails uniquement, du sujet, et du messsage
 with MailBox('imap.gmail.com').login('XXXX@gmail.com', 'PASSWORD') as mailbox:
     for msg in mailbox.fetch(AND(seen=False)):
         print(msg.from_, msg.subject, msg.text)
