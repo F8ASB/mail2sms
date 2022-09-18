a53ddae790c90caada8caa10e9fc2b24b9247b59
F8ASB <f8asb@hotmail.fr>
2022-09-18 17:49:32 +0200

    Create Phonelist.py

---
+++ Phonelist.py
@@ -0,0 +1,16 @@
+Numlist = []
+UserListe = ['f8asb@hotmail.fr','juan']
+
+Phonelist = {
+        'friends':{
+            'Juan': +33612345678,
+            'Pauline': +33612345678,
+            'Thibaut': +33612345678,
+            'Cethymene': +33612345678
+        },
+
+        'sysops':{
+            'F8XXX': +33612345678,
+            'F4XXX': +33612345678
+        },
+        }
